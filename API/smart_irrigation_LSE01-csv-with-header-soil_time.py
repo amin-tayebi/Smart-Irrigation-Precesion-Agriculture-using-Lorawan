7b5ccb1d2f744833fbe72ab7c43d4f49a3bdd750
Amin TAYEBI <amin.tayebi313@gmail.com>
2022-09-29 20:29:50 +0200

    Update smart_irrigation_LSE01-csv-with-header-soil_time.py

--- API/smart_irrigation_LSE01-csv-with-header-soil_time.py
+++ API/smart_irrigation_LSE01-csv-with-header-soil_time.py
@@ -1,3 +1,23 @@
+#*******************************************************************************
+# Copyright (c) 2022 amin TAYEBI
+#
+# Permission is hereby granted, free of charge, to anyone
+# obtaining a copy of this document and accompanying files,
+# to do whatever they want with them without any restriction,
+# including, but not limited to, copying, modification and redistribution.
+# NO WARRANTY OF ANY KIND IS PROVIDED.
+#
+# Setup invironment to run:
+# The script should be run on venv mode in python3
+# 
+# Functionalities:
+# - This mosule read sensor dragino LSE01 data and if "moisture is below 30%" open the Actuator (solenoid valve) for 2 seconds (defined on actuator downlink)
+# - Reapeats forever
+# - Works also with moisture value even 0  or 100 while previus versions just worked with moisture value 10-99
+# - Save theese data to CSV file in currebt path: 
+# i) soil humidity from Adafruite soil sensor 
+# ii) date/time of data captured 
+# *******************************************************************************/
 #!/usr/bin/env python3
 import random
 from paho.mqtt import client as mqtt_client
@@ -8,11 +28,11 @@ import csv
 
 broker = 'eu1.cloud.thethings.network'
 port = 1883
-topic = "v3/app-3@ttn/devices/eui-a840419e918220ed-lse01/up"
+topic = "v3/<app name>@ttn/devices/<sensor dev eui>/up"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
-username = 'app-3'
-password = 'NNSXS.MVFULYOGEG4B5XBPRN4WHC2QHRF2IPSBDAQ7PVA.TF65V52GWNEIE7WAGQX6RHIK6RDVDBN3UXONDZKTN6DWYE4L2PAQ'
+username = '<app name>'
+password = 'NNSXS.XXX'
 
 
 
@@ -80,11 +100,11 @@ def subscribe(client: mqtt_client):
                 print(payload2, "%:   In LSE01 sensor moisture is below 30%")
 
                 # "Ag==" is euqal to 02 which defined on actuator if this downlink received open the valve for 3 seconds
-                publish.single("v3/arduinosdi1222222222222222@ttn/devices/eui-70b3d57ed0054220/down/push",
+                publish.single("v3/<app name>@ttn/devices/<actuator dev eui>/down/push",
                                '{"downlinks":[{"f_port": 16,"frm_payload":"Ag==","priority": "NORMAL"}]}',
                                hostname="eu1.cloud.thethings.network", port=1883,
-                               auth={'username': "arduinosdi1222222222222222",
-                                     'password': "NNSXS.RDHIBKJLMCRK2TQOVZMB35HYHIZZHRLNUBU3FRA.US4FXZIZW7DGKFWCZOKPX7BKGMG4TESJXN26OJPP5BPMO3PFVZCA"})
+                               auth={'username': "<app name>",
+                                     'password': "NNSXS.XXX"})
 
 
         except ValueError:
