7ad847884466568747a06d495f9ffd89a50785b2
Amin TAYEBI <amin.tayebi313@gmail.com>
2022-09-30 20:06:55 +0200

    Update adafruite decoder 2.js

--- Decoders_js/adafruite decoder 2.js
+++ Decoders_js/adafruite decoder 2.js
@@ -1,6 +1,7 @@
+//shows "Moisture_percentage" and ""Last_Valve_mode
 function Decoder(bytes) {
       var data = {};
 	data.Moisture_percentage =  bytes[0] ;
-	data.Valve_mode =  bytes[4]  ;
+	data.Last_Valve_mode =  bytes[4]  ;
     return  data;
   }
